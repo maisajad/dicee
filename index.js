328bbd1815e1278bbb0ae5fe038730b5ba83afa4
maisajad <72973453+maisajad@users.noreply.github.com>
2021-03-29 20:13:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var randomNumber1=Math.floor((Math.random()*6)+1);
-var randomeDiceImg="images/dice"+randomNumber1+".png";
+var randomeDiceImg="dice"+randomNumber1+".png";
 var leftImg=document.querySelectorAll("img")[0];
 leftImg.setAttribute("src",randomeDiceImg);
 var randomNumber2=Math.floor((Math.random()*6+1));
-var randomeDiceImg2="images/dice"+randomNumber2+".png";
+var randomeDiceImg2="dice"+randomNumber2+".png";
 var rightImg=document.querySelectorAll("img")[1];
 rightImg.setAttribute("src",randomeDiceImg2);
 if(randomNumber1>randomNumber2)
